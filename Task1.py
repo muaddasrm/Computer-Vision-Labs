89a8ceed758a285d10166861fd32d888b674eb44
Muhammad Mudassar <muaddasr.m@gmail.com>
2022-08-25 22:54:49 +0500

    Task1 completed with capture video code.

--- Task1.py
+++ Task1.py
@@ -31,5 +31,21 @@ img3 = cv2.imread("grayscale.jpg")
 img4 = cv2.imread("back_to_rgb.jpg")
 horizontal_concat = np.concatenate((img1, img2, img3, img4), axis=1)
 cv2.imshow("Horizontal plot", horizontal_concat)
-cv2.waitKey(50000)  # waits until a key is pressed
+cv2.waitKey(500)  # waits until a key is pressed
 cv2.destroyAllWindows()  # destroys the window showing image
+
+# Capture a video using OpenCV
+cap= cv2.VideoCapture(0)
+width= int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+height= int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+writer= cv2.VideoWriter('basicvideo.mp4', cv2.VideoWriter_fourcc(*'DIVX'), 20, (width,height))
+
+while True:
+    ret,frame= cap.read()
+    writer.write(frame)
+    cv2.imshow('frame', frame)
+    if cv2.waitKey(1) & 0xFF == 27:
+        break
+cap.release()
+writer.release()
+cv2.destroyAllWindows()
